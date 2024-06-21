82a441a78874ac9b767d92811bb0b14d2d50583b
Andy Park <chanhyup@andrew.cmu.edu>
2024-06-21 14:50:22 -0400

    Add constant

--- script/constants.py
+++ script/constants.py
@@ -5,4 +5,5 @@ NZ_PAIRS = int(NZ_BINS * (NZ_BINS + 1) / 2)
 BIN_PAIRS = [(i, j) for i in range(NZ_BINS) for j in range(i, NZ_BINS)]
 BP2INDEX = {bp: i for i, bp in enumerate(BIN_PAIRS)}
 STR2DEG = 4 * np.pi * (180 / np.pi) ** 2
+ARCMIN2RAD = 1.0 / 60.0 * np.pi / 180.0
 FSKY = 416.0 / STR2DEG
